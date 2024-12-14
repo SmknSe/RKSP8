b68981ec4ff1a6c6f0a2db1cccb8cdea903d9a33
whynot <whynot.7@mail.ru>
2024-12-14 22:46:17 +0300

    feat: userDTO added

--- src/main/java/com/example/composer/configuration/FeignConfig.java
+++ src/main/java/com/example/composer/configuration/FeignConfig.java
@@ -15,7 +15,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScans(value = {
         @ComponentScan(basePackages = {
-                "com.example.composer.api",
+                "api",
         })
 })
 public class FeignConfig {
